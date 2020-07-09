c9834a22e7adda70ff2f5f55fb9dc218c44e60c2
Ivan1874 <ivan51673@gmail.com>
2020-07-09 17:16:50 +0300

    Telegram markdown syntax using code-friendly

--- lib/markdown2.py
+++ lib/markdown2.py
@@ -1961,7 +1961,7 @@ class Markdown(object):
     _strong_re = re.compile(r"(\*\*|__)(?=\S)(.+?[*_]*)(?<=\S)\1", re.S)
     _em_re = re.compile(r"(\*|_)(?=\S)(.+?)(?<=\S)\1", re.S)
     _code_friendly_strong_re = re.compile(r"\*\*(?=\S)(.+?[*_]*)(?<=\S)\*\*", re.S)
-    _code_friendly_em_re = re.compile(r"\*(?=\S)(.+?)(?<=\S)\*", re.S)
+    _code_friendly_em_re = re.compile(r"__(?=\S)(.+?)(?<=\S)__", re.S)
     def _do_italics_and_bold(self, text):
         # <strong> must go first:
         if "code-friendly" in self.extras:
